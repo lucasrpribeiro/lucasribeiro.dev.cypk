4c8065d4ebee935bdf7e40f0a1eef2acdf0346e5
Lucas <lucas.ribeiro711@gmail.com>
2024-08-21 14:59:09 -0300

    fix: :floppy_disk: selection and scroll bar

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div style="background-image: radial-gradient(ellipse at 33% 10%, #461616 0%, transparent 75%); background-repeat: no-repeat;" class="my-body selection:bg-tertiary-500 selection:text-white selection:shadow-white flex flex-col justify-start items-center h-screen w-full bg-background-500 text-primary-500">
+  <div style="background-image: radial-gradient(ellipse at 33% 10%, #461616 0%, transparent 75%); background-repeat: no-repeat;" class="my-body flex flex-col justify-start items-center h-screen w-full bg-background-500 text-primary-500">
     <NavBar />
     <HeroSection/>
   </div>
