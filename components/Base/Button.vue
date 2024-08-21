2e1067741cee0fd27948d8ec735c259da5e17b3a
Lucas <lucas.ribeiro711@gmail.com>
2024-08-21 15:33:02 -0300

    chore: :floppy_disk: some things

--- components/Base/Button.vue
+++ components/Base/Button.vue
@@ -1,5 +1,5 @@
 <template>
-  <button type="button" class="border border-[transparent] p-2 text-secondary-500 font-semibold hover:bg-primary-900 hover:border-secondary-500 hover:text-tertiary-500 ">
+  <button type="button" class="border border-[transparent] p-2 text-secondary-500 font-semibold hover:bg-primary-900 hover:border-secondary-500 hover:text-tertiary-500">
     <slot/>
   </button>
 </template>
--- components/HeroSection.vue
+++ components/HeroSection.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="container mx-auto px-5 mt-9 flex items-center justify-between">
     <div class="flex-1 flex flex-col gap-2 items-start">
-      <h1 class="text-6xl font-semibold">
-        Olá, eu sou Lucas!
+      <h1 class="text-6xl text-tertiary-500 font-semibold">
+        Olá, eu sou <span class="text-secondary-500 font-['VT323'] text-7xl font-thin">Lucas_</span>!
         Programador Web
       </h1>
       <p class="text-3xl">
