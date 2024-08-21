9347818acd0823b7ac688151cc2966aa4f007923
Lucas <lucas.ribeiro711@gmail.com>
2024-08-20 22:07:57 -0300

    feat: :floppy_disk: install and use new fonts

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -11,10 +11,10 @@ const links = ref([
   <nav class="relative container px-6 mx-auto flex items-center h-20">
     <div class="absolute bottom-0 left-0 right-0 h-[2px] neon-border-red"></div>
     <div class="relative z-10 w-full flex items-center justify-between text-primary-500">
-      <span>LKs</span>
+      <span class="logo text-4xl">LUCAS RIBEIRO</span>
       <ul class="flex gap-3">
         <li v-for="(link, link_index) in links" :key="`${link_index}-link`">
-          <a href="" class="hover:text-primary-200 duration-300">{{ link.name }}</a>
+          <a href="" class="hover:text-primary-200 duration-300 uppercase">{{ link.name }}</a>
         </li>
       </ul>
     </div>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,19 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   css: ['@/assets/main.css'],
-  modules: ["@nuxtjs/tailwindcss"]
+  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/google-fonts"],
+
+  googleFonts: {
+    overwriting: true,
+    families: {
+      'Rajdhani': {
+        wght: [400, 500, 600, 700, 800],
+      },
+      'VT323': {
+        wght: [400, 500, 600, 700, 800],
+      },
+    },
+    display: 'swap',
+  },
+
 })
