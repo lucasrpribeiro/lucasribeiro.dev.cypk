195d3ba6dbd294f73979303325d6ffa332a402c5
Lucas <lucas.ribeiro711@gmail.com>
2024-08-21 13:02:24 -0300

    fear: :floppy_disk: hover effect

--- components/Base/Button.vue
+++ components/Base/Button.vue
@@ -1,5 +1,5 @@
 <template>
-  <button type="button" class="border border-[transparent] p-2 text-secondary-500 font-semibold hover:bg-primary-900 hover:border-primary-300 duration-300">
+  <button type="button" class="border border-[transparent] p-2 text-secondary-500 font-semibold hover:bg-primary-900 hover:border-secondary-500 hover:text-tertiary-500 ">
     <slot/>
   </button>
 </template>
